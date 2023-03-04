4277ab19d11ba65534280fc5ca0d234626d68dba
Saharat Shoedsuk <6310110501@psu.ac.th>
2023-03-04 18:29:50 +0700

    first commit

---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+
+export const Product = (props) => {
+  const { id, productName, productImage } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const cartItemCount = cartItems[id];
+
+  return (
+    <div className="product">
+      <img src={productImage} />
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
+      </button>
+    </div>
+  );
+};
---
+++ src/products.js
@@ -0,0 +1,47 @@
+import product1 from "./assets/products/weed1.png";
+import product2 from "./assets/products/weed2.png";
+import product3 from "./assets/products/weed3.png";
+import product4 from "./assets/products/weed4.png";
+import product5 from "./assets/products/weed5.png";
+import product6 from "./assets/products/weed6.png";
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "LEMON SKUNK",
+    price: 500.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "BLUE DREAM",
+    price: 500.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "UNICORN POOP",
+    price: 500.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "SPINACH",
+    price: 210.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "XMG (TRUSS)",
+    price: 220.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "TWEED",
+    price: 875.0,
+    productImage: product6,
+  },
+
+];
